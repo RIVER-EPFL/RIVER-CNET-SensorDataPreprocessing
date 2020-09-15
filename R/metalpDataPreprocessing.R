8ee6a6a539c95c8f482dda57a52c95aa4474cdf6
Mathieu Clément <mcfly1893@gmail.com>
2020-09-15 09:51:54 +0200

    Create bases of the gadget UI

--- R/metalpDataPreprocessing.R
+++ R/metalpDataPreprocessing.R
@@ -5,12 +5,40 @@
 #'
 #' @export
 metalpDataPreprocessing <- function() {
-  ui <- miniPage()
+  ui <- miniPage(
+    includeCSS(system.file("www/main.css", package = "metalpDataPreprocessing")),
+    gadgetTitleBar('Sensors raw data processing', left = miniTitleBarCancelButton(), right = NULL),
+    div(
+      class = 'content-wrapper',
+      fillCol(
+        selectizeInput(
+          inputId =  'sites',
+          label = 'Stations',
+          choices = c('VAD', 'VAU', 'VEL', 'FED', 'FEU', 'PEU', 'AND', 'ANU', 'RIC', 'VID', 'VIM', 'VIU'),
+          multiple = TRUE,
+          options = list(
+            'placeholder' = 'Select some stations...',
+            'plugins' = list('remove_button')
+          )
+        ),
+        selectizeInput(
+          inputId =  'sites',
+          label = 'Stations',
+          choices = c('BP', 'CO2atm', 'TURB', 'DO', 'CDOM', 'COND', 'PAR1', 'PAR2', 'DEPTH', 'pCO2'),
+          multiple = TRUE,
+          options = list(
+            'placeholder' = 'Select some stations...',
+            'plugins' = list('remove_button')
+          )
+        )
+      )
+    )
+  )
 
   server <- function(input, output, session) {
 
   }
 
-  viewer <- dialogViewer()
+  viewer <- dialogViewer('METALP Data Preprocessing')
   runGadget(ui, server, viewer = viewer)
 }
