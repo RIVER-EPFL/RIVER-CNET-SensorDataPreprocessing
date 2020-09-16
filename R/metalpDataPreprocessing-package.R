e372bb319290378e7dc1f61a8a05be1f85fbb93f
Mathieu Clément <mcfly1893@gmail.com>
2020-09-16 10:31:02 +0200

    Fill DESCRIPTION, add LICENSE, and create NAMESPACE with external packages

---
+++ R/metalpDataPreprocessing-package.R
@@ -0,0 +1,8 @@
+#' METALP Sensor Data Preprocessing
+#'
+#' Use this addin to perform all the data preprocessing for the METALP sensors data
+#'
+#' @name metalpDataPreprocessing
+#' @docType package
+#' @import shiny miniUI shinyFiles jsonlite zoo data.table lubridate magrittr dplyr purrr
+NULL
