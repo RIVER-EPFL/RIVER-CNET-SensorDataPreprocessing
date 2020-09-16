643922a57c6d24d0326746e3e47493365f933d7a
Mathieu Clément <mcfly1893@gmail.com>
2020-09-16 10:51:57 +0200

    Add some internal data

---
+++ data-raw/parametersInfo.R
@@ -0,0 +1,11 @@
+## code to prepare `parametersInfo`
+## Sensors data parameters info used to look for and combine the different output files together
+
+parametersInfo <- data.frame(
+  'name' = c('BP', 'CO2atm', 'TURB', 'DO', 'CDOM', 'COND', 'PAR1', 'PAR2', 'DEPTH', 'pCO2'),
+  'pattern' = c('_BP.csv$', '_CO2ATM.LOG$', 'Cat.TXT$', 'Cat.TXT$', 'Cat.TXT$', '_COND.csv$', '_PAR1.csv$', '_PAR2.csv$', '_NDEPTH.csv$', '_PCO2.csv$'),
+  'subpattern' = c('NA', 'NA', '/TURB/', '/DO/', '/CDOM/', 'NA', 'NA', 'NA', 'NA', 'NA'),
+  'parsingFunc' = c('parseBP', 'parseCO2ATM', 'parseTURB', 'parseDO', 'parseCDOM', 'parseCOND', 'parsePAR1', 'parsePAR2', 'parseNDEPTH', 'parsePCO2')
+)
+
+usethis::use_data(parametersInfo, overwrite = TRUE, internal = TRUE)
