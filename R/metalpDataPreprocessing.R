ebc1bef88cc26798e7fbfc64179fec737bd827c8
Mathieu Clément <mcfly1893@gmail.com>
2020-09-22 10:32:03 +0200

    Add all and clear buttons to inputs selection

--- R/metalpDataPreprocessing.R
+++ R/metalpDataPreprocessing.R
@@ -17,7 +17,7 @@ metalpDataPreprocessing <- function() {
       div(
         selectizeInput(
           inputId =  'sites',
-          label = 'Stations',
+          label = span('Stations', actionButton('allSites', 'All'), actionButton('clearSites', 'Clear')),
           choices = c('VAD', 'VAU', 'VEL', 'FED', 'FEU', 'PEU', 'AND', 'ANU', 'RIC', 'VID', 'VIM', 'VIU'),
           multiple = TRUE,
           options = list(
@@ -27,7 +27,7 @@ metalpDataPreprocessing <- function() {
         ),
         selectizeInput(
           inputId =  'parameters',
-          label = 'Parameters',
+          label = span('Parameters', actionButton('allParameters', 'All'), actionButton('clearParameters', 'Clear')),
           choices = c('BP', 'CO2atm', 'TURB', 'DO', 'CDOM', 'COND', 'PAR1', 'PAR2', 'DEPTH', 'pCO2'),
           multiple = TRUE,
           options = list(
@@ -56,11 +56,37 @@ metalpDataPreprocessing <- function() {
   server <- function(input, output, session) {
     roots <- c('root' = '/', 'home' = '~', 'working directory' = '.')
 
+
+    ## Directories slection #######################################################
+
     shinyDirChoose(input, 'inputDir', roots = roots, defaultRoot = 'working directory')
     shinyDirChoose(input, 'outputDir', roots = roots, defaultRoot = 'working directory')
 
     output$selectedInputDir <- renderText(parseDirPath(roots, input$inputDir))
     output$selectedOutputDir <- renderText(parseDirPath(roots, input$outputDir))
+
+
+    ## Stations and Parameters selection ##########################################
+
+    observeEvent(input$allSites, ignoreInit = TRUE, {
+      updateSelectizeInput(session, 'sites', selected = c('VAD', 'VAU', 'VEL', 'FED', 'FEU', 'PEU', 'AND', 'ANU', 'RIC', 'VID', 'VIM', 'VIU'))
+    })
+
+    observeEvent(input$allParameters, ignoreInit = TRUE, {
+      updateSelectizeInput(session, 'parameters', selected = c('PAR1', 'PAR2', 'BP', 'CO2atm', 'COND', 'DO', 'CDOM', 'TURB', 'pCO2', 'DEPTH'))
+    })
+
+    observeEvent(input$clearSites, ignoreInit = TRUE, {
+      updateSelectizeInput(session, 'sites', selected = '')
+    })
+
+    observeEvent(input$clearParameters, ignoreInit = TRUE, {
+      updateSelectizeInput(session, 'parameters', selected = '')
+    })
+
+
+
+    ## Script running #############################################################
 
     observeEvent(input$run, ignoreInit = TRUE, {
       sendUpdateToConcole(id = 'console-raw', action = 'clear')
